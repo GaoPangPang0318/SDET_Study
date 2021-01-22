7d33e1b509e21832708680988849df736e8bc29e
GaoPangPang0318 <719453296@qq.com>
2021-01-22 16:57:41 +0800

    添加多浏览器

--- study_selenium/Base.py
+++ study_selenium/Base.py
@@ -1,10 +1,20 @@
 from  selenium import webdriver
-
+import os
 class Base():
     def setup(self):
+        option = webdriver.ChromeOptions()
+        option.add_experimental_option('w3c', False)
+
+        browser=os.getenv("brwser")  #参数从哪儿来，怎么来呢？？？
+        print("broswer",browser)
+        if browser=="firefox":
+            self.driver=webdriver.Firefox()
+        elif browser =='headless':
+            self.driver=webdriver.PhantomJS()
+        else:
+            self.driver = webdriver.Chrome(options=option)
         option=webdriver.ChromeOptions()
         option.add_experimental_option('w3c',False)
-        self.driver=webdriver.Chrome(options=option)
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
 
