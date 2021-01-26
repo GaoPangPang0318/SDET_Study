31c070c39e903110f8d97c8d5f3989f45aa97953
GaoPangPang0318 <719453296@qq.com>
2021-01-26 22:17:10 +0800

    appium-APP控件定位示例

--- study_appium/test_dw.py
+++ study_appium/test_dw.py
@@ -12,7 +12,7 @@ class TestDw:
         desired_caps['appActivity']='.view.WelcomeActivityAlias'
         desired_caps['noResrt']=True
         #首次运行APP后不对APP进行重置操作
-        desired_caps['dontStopAppOnReset']=True
+        #desired_caps['dontStopAppOnReset']=True
         #跳过app初始化需要获取权限的操作
         desired_caps['skipDeviceInintialization']=True
         # send_keys的值为中文的话得有以下两个条件
@@ -26,6 +26,7 @@ class TestDw:
     def teardown(self):
         self.driver.quit()
 
+    @pytest.mark.skip
     def test_dw(self):
         print('搜索测试')
         """
@@ -48,6 +49,31 @@ class TestDw:
 
         #断言操作
         assert  current_price>200
+
+    def test_attribute(self):
+        """
+            1.打开雪球应用
+            2.定位首页搜索框
+            3.判断搜索框是否可用，并检查搜索框框name属性值
+            4.打印搜索框这个元素的左上角坐标和他的宽高
+            5.向搜索框输入：alibab
+            6.判断【阿里巴巴】是否可见
+            7.如果可见，打印搜索成功，否则打印“搜索失败
+        """
+        #定位搜索框元素
+        el_search=self.driver.find_element_by_id('com.xueqiu.android:id/tv_search')
+        print("搜索框name：",el_search.text)
+        print("搜索框坐标：",el_search.location)
+        print("搜索框宽高：",el_search.size)
+        if el_search.is_enabled():
+            el_search.click()
+            self.driver.find_element_by_id('com.xueqiu.android:id/search_input_text').send_keys('alibaba')
+            alibaba_el=self.driver.find_element_by_xpath("//*[@resource-id='com.xueqiu.android:id/name' and @text='阿里巴巴']")
+            el_display=alibaba_el.get_attribute("displayed")
+            if el_display=="true":
+                print("搜索成功")
+            else:
+                print("搜索失败")
 
 if __name__ == '__main__':
     pytest.main()
